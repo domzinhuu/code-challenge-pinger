8cbc9abd87fa65559f038f61b6324db4d6d75afd
Maique Rosa <maique.rosa@bitz.com.br>
2022-02-23 01:40:03 -0300

    feat: add getCompetitionById and getCompetitionStanding to CompetitionService

--- src/app/services/competition/competition.service.ts
+++ src/app/services/competition/competition.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { Competition } from 'src/app/models/competition';
+import { CompetitionStanding } from 'src/app/models/standing';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,17 @@ export class CompetitionService {
     return this.httpClient
       .get<any>(`${environment.apiUrl}/competitions?plan=TIER_ONE`)
       .pipe(map((res) => res.competitions));
+  }
+
+  public getCompetitionById(id: number): Observable<Competition> {
+    return this.httpClient.get<Competition>(
+      `${environment.apiUrl}/competitions/${id}`
+    );
+  }
+
+  public getCompetitionStanding(id: number): Observable<CompetitionStanding[]> {
+    return this.httpClient
+      .get<any>(`${environment.apiUrl}/competitions/${id}/standings`)
+      .pipe(map((res) => res.standings));
   }
 }
