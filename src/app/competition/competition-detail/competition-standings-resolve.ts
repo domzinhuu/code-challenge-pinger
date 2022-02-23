f2cec4f9377ada0cdc5db79279f9ccb34aab3bb1
Maique Rosa <maique.rosa@bitz.com.br>
2022-02-23 01:55:58 -0300

    refactor: adjust resolve to treate 404 error

--- src/app/competition/competition-detail/competition-standings-resolve.ts
+++ src/app/competition/competition-detail/competition-standings-resolve.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { EmptyError, Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Competition } from 'src/app/models/competition';
 import { CompetitionStanding } from 'src/app/models/standing';
 import { CompetitionService } from 'src/app/services/competition/competition.service';
 
 @Injectable({ providedIn: 'root' })
-export class CompetitionStandingResolver implements Resolve<CompetitionStanding[]> {
+export class CompetitionStandingResolver
+  implements Resolve<CompetitionStanding[]>
+{
   constructor(private competitionService: CompetitionService) {}
   resolve(route: ActivatedRouteSnapshot): Observable<CompetitionStanding[]> {
     const competitionID = Number(route.params.id);
-    return this.competitionService.getCompetitionStanding(competitionID);
+    return this.competitionService
+      .getCompetitionStanding(competitionID)
+      .pipe(catchError((err) => of([])));
   }
 }
